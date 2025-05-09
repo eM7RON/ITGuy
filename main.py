f737cdd60a5fdf56c0639b02ce6b63d0677771de
Munkytron <me@simontucker.one>
2025-05-09 20:51:02 +0100

    corrected case for some variables

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import time
 
 BOT_TOKEN       = os.environ["BOT_TOKEN"]
 SIGNING_SECRET  = os.environ["SIGNING_SECRET"]
-WORKSPACE_ID    = os.environ["WORKSPACE_ID"]
-CHANNEL_ID      = os.environ["CHANNEL_ID"]
+WORKSPACE_ID    = os.environ["workspace_id"]
+CHANNEL_ID      = os.environ["channel_id"]
 
 def http_helper(url, method='post', query={}, body=''):
     headers = {"Authorization": f"Bearer {bot_token}"}
@@ -33,36 +33,36 @@ def authping():
     "Check if the bot token is valid"
     return req('https://slack.com/api/auth.test')['ok']
      
-def conv_members(CHANNEL_ID, cursor='', limit=100, paginate='members'):
+def conv_members(channel_id, cursor='', limit=100, paginate='members'):
     return req('https://slack.com/api/conversations.members', query={
-        'channel': CHANNEL_ID, 'cursor': cursor, 'limit': limit}, paginate=paginate)
+        'channel': channel_id, 'cursor': cursor, 'limit': limit}, paginate=paginate)
 
-def conv_history(CHANNEL_ID, cursor='', limit=100, paginate='messages'):
+def conv_history(channel_id, cursor='', limit=100, paginate='messages'):
     return req('https://slack.com/api/conversations.history', query={
-        'channel': CHANNEL_ID, 'cursor': cursor, 'limit': limit}, paginate=paginate)
+        'channel': channel_id, 'cursor': cursor, 'limit': limit}, paginate=paginate)
 
-def conv_replies(CHANNEL_ID, timestamp, cursor='', limit=100, paginate='messages'):
+def conv_replies(channel_id, timestamp, cursor='', limit=100, paginate='messages'):
     "Retrieve a thread of messages posted to a conversation"
     return req('https://slack.com/api/conversations.replies', 'get', query={
-        'channel': CHANNEL_ID, 'ts': timestamp, 'cursor': cursor, 'limit': limit}, paginate=paginate)
+        'channel': channel_id, 'ts': timestamp, 'cursor': cursor, 'limit': limit}, paginate=paginate)
 
-def chat_post_message(CHANNEL_ID, query={}):
-    req('https://slack.com/api/chat.postMessage', query={**query, 'channel': CHANNEL_ID})
+def chat_post_message(channel_id, query={}):
+    req('https://slack.com/api/chat.postMessage', query={**query, 'channel': channel_id})
 
-def chat_me_message(CHANNEL_ID, text=''):
-    req('https://slack.com/api/chat.meMessage', query={'channel': CHANNEL_ID, 'text': text})
+def chat_me_message(channel_id, text=''):
+    req('https://slack.com/api/chat.meMessage', query={'channel': channel_id, 'text': text})
 
-def chat_post_ephemeral(CHANNEL_ID, user_id, query={}):
-    req('https://slack.com/api/chat.postEphemeral', query={**query, 'channel': CHANNEL_ID, 'user': user_id})
+def chat_post_ephemeral(channel_id, user_id, query={}):
+    req('https://slack.com/api/chat.postEphemeral', query={**query, 'channel': channel_id, 'user': user_id})
 
-def chat_delete(CHANNEL_ID, timestamp, query={}):
-    req('https://slack.com/api/chat.delete', query={**query, 'channel': CHANNEL_ID, 'ts': timestamp})
+def chat_delete(channel_id, timestamp, query={}):
+    req('https://slack.com/api/chat.delete', query={**query, 'channel': channel_id, 'ts': timestamp})
 
-def chat_get_permalink(CHANNEL_ID, timestamp, query={}):
-    return req('https://slack.com/api/chat.getPermalink', query={**query, 'channel': CHANNEL_ID, 'message_ts': timestamp}).json()
+def chat_get_permalink(channel_id, timestamp, query={}):
+    return req('https://slack.com/api/chat.getPermalink', query={**query, 'channel': channel_id, 'message_ts': timestamp}).json()
 
-def chat_update(CHANNEL_ID, timestamp, query={}):
-    return req('https://slack.com/api/chat.update', query={**query, 'channel': CHANNEL_ID, 'ts': timestamp})
+def chat_update(channel_id, timestamp, query={}):
+    return req('https://slack.com/api/chat.update', query={**query, 'channel': channel_id, 'ts': timestamp})
 
 def users_info(user_id, query={}, method='get'):
     return req('https://slack.com/api/users.info', query={**query, 'user': user_id}, method=method)
